55b61300819440a901873ba0e9655cb682e7ff1c
Anand Bhat <anand.bhat@gmail.com>
2022-02-15 18:13:59 +0530

    Update bar_chart_correct.go

--- bar_chart_correct.go
+++ bar_chart_correct.go
@@ -1,3 +1,5 @@
+// Find the maximum area formed in the bar graph
+
 package main
 
 import (
@@ -12,10 +14,16 @@ func min(i,j int) int {
   return j
 }
 
-func minMax(arr []int) (int, int) {
+// Logic:
+// Use two indixes: left and right
+// left initialized to first element and right initialized to last element
+// Iterate over the array and find the area formed between left and right
+// Area is calculated as min(arr[left], arr[right]) * (right - left)
+// increment left and update max if bigger area is found
+
+func maxArea(arr []int) int {
   left := 0
   right := len(arr) - 1
-  minArea := 0
   maxArea := 0
 
   for  {
@@ -23,47 +31,15 @@ func minMax(arr []int) (int, int) {
       break
     }
     area := min(arr[left], arr[right]) * (right - left)
-    if area <= minArea {
-      minArea = area
-    }
     if area > maxArea {
       maxArea = area
     }
     left ++
   }
-  return minArea, maxArea
+  return  maxArea
 }
 
 func main() {
   arr := []int {6, 9, 3, 4, 5, 8}
-  min, max := minMax(arr)
-  fmt.Println("min and max areas = ", min, max)
-
-  arr = []int {7, 1, 2, 3, 9}
-  min, max = minMax(arr)
-  fmt.Println("min and max areas = ", min, max)
-
-  arr = []int {}
-  min, max = minMax(arr)
-  fmt.Println("min and max areas = ", min, max)
-
-  arr = []int {1}
-  min, max = minMax(arr)
-  fmt.Println("min and max areas = ", min, max)
-
-  arr = []int {3, 4}
-  min, max = minMax(arr)
-  fmt.Println("min and max areas = ", min, max)
-
-  arr = []int {9, 8, 7, 6, 5, 4, 3, 2, 1}
-  min, max = minMax(arr)
-  fmt.Println("min and max areas = ", min, max)
-
-  arr = []int {1, 2, 3, 4, 5, 6, 7, 8, 9}
-  min, max = minMax(arr)
-  fmt.Println("min and max areas = ", min, max)
-
-  arr = []int {4, 8, 1, 2, 3, 9}
-  min, max = minMax(arr)
-  fmt.Println("min and max areas = ", min, max)
+  fmt.Println("max area = ", maxArea(arr))
 }
