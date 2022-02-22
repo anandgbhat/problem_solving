cc51de4623bbedd4b733ae1356e196ed02eaad67
Anand Bhat <anand.bhat@gmail.com>
2022-02-22 22:16:43 +0530

    Add files via upload

--- string_backspace_match.go
+++ string_backspace_match.go
@@ -9,26 +9,34 @@ import (
 // abc##d is same as ad
 
 func isMatch(s1,s2 string) bool {
+  // Validation
+  if len(s1) == 0 && len(s2) == 0 {
+    fmt.Println("Invalid strings")
+    return false
+  }
+
   // Convert the string into rune
   s1Rune := []rune(s1)
   var str []rune
   j := 0
-  var validChar bool
+  var validCharFound bool  // If any valid char found, will be set to true
 
   // # character is 35
   // Walk through the rune slice and get rid of previous characters
   for _, val := range s1Rune {
     if val != 35 {
       str = append(str, val)
-      validChar = true
+      validCharFound = true
       j++
     } else {
-      if validChar != true {
+      if validCharFound != true {
         // No valid characters seen so far
         // Skip the # and move forward
         continue
       }
-      j--
+      if j > 0 {
+        j--
+      }
       str = str[0:j]
     }
   }
@@ -40,4 +48,6 @@ func main() {
   fmt.Println("Match abc##c and ac = ", isMatch("abc##c", "ac"))
   fmt.Println("Match ab#c and abc = ", isMatch("ab#c", "abc"))
   fmt.Println("Match ##ab#c and abc = ", isMatch("##abb#c", "abc"))
+  fmt.Println("Match ab## and nullstr = ", isMatch("ab##", ""))
+  fmt.Println("Match a###b and b = ", isMatch("a###b", "b"))
 }
