b4fce45cf53c357e4eef80b8c54196df6d93b67a
Anand Bhat <anand.bhat@gmail.com>
2022-02-26 11:20:44 +0530

    Add files via upload

---
+++ binary_tree_sum_of_all_nodes.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+  "fmt"
+)
+
+type treeNode struct {
+  val int
+  left *treeNode
+  right *treeNode
+}
+
+var total int
+
+func (root *treeNode) sumOfAllValues() int {
+  if root == nil {
+    return 0
+  }
+  total += root.val
+  (*treeNode).sumOfAllValues(root.left)
+  (*treeNode).sumOfAllValues(root.right)
+  return total
+}
+
+func main() {
+  root := new(treeNode)
+  root.val = 3
+
+  root.right = new(treeNode)
+  root.right.val = 4
+
+  root.left = new(treeNode)
+  root.left.val = 11
+
+  root.left.left = new(treeNode)
+  root.left.left.val = 4
+
+  root.left.right = new(treeNode)
+  root.left.right.val = 2
+
+  root.right.right = new(treeNode)
+  root.right.right.val = 1
+
+  fmt.Println("sum = ", (*treeNode).sumOfAllValues(root))
+}
