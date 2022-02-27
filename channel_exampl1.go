498ef0a6f938b641b3931a08c48f80d4f2cb0134
Anand Bhat <anand.bhat@gmail.com>
2022-02-27 16:48:50 +0530

    Add files via upload

---
+++ channel_exampl1.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+  "fmt"
+  "sync"
+)
+
+var wg sync.WaitGroup
+
+func display(words chan string) {
+  defer wg.Done()
+  str := <- words
+  fmt.Println(str)
+  words <- "received"
+}
+
+func main() {
+  words := make(chan string)
+  wg.Add(1)
+  go display(words)
+  words <- "Sending encoded messages"
+  fmt.Println(<-words)
+  close(words)
+  wg.Wait()
+  _, _ = fmt.Scanln()
+}
+
