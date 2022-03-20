8789e0c5926d5863b5c9e422248b3b85a8dc23ff
Anand Bhat <anand.bhat@gmail.com>
2022-03-20 13:50:24 +0530

    Update multi_level_doubly_linked_list_merge.go

--- multi_level_doubly_linked_list_merge.go
+++ multi_level_doubly_linked_list_merge.go
@@ -13,17 +13,24 @@ func flatten(head *DoublyLinkedList) *DoublyLinkedList {
 
   tmp := head
 
-  // Walk through the list till we hit nil
+  
   for tmp != nil {
+    // Advance the list if there are no child pointers in the node
     if tmp.child == nil {
       tmp = tmp.next
     } else {
+      // Child pointer. Flatten the child list as is.
+      // Ignore any child pointers in the child list as it will get processed in
+      // the next iteration of main loop
       tail := tmp.child
+      // Traverse the child list till end
       for tail.next != nil {
         tail = tail.next
       }
 
+      // Adjust all pointers to add the child list to main list
       tail.next = tmp.next
+      // Handle the case wherein last node has child pinter
       if tail.next != nil {
         tail.next.prev = tail
       }
