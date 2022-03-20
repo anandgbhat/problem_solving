3e52677ab25e546986d0fa6b99bd49ed4bbd209c
Anand Bhat <anand.bhat@gmail.com>
2022-03-20 14:57:08 +0530

    Add files via upload

---
+++ reverse_singly_linked_list.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "fmt"
+)
+
+type node struct {
+  val int
+  node *next
+}
+
+func reverseList(head *node) *node {
+  // Base condition
+  if head == nil || head->next == nil {
+    return head
+  }
+  prev := nil
+  current := head
+
+  for current != nil {
+    next = current.next
+    current.next = prev
+    prev = current
+    current = next
+  }
+
+  return prev
+}
+
+
